3e2f9ed7ab4f84f6187b330d6d32f2a0c14fbd9f
Kiril <kirilltrush2001@gmail.com>
2026-07-27 18:57:52 +0300

    Add a code for police blinker

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+#define LED_OUT_B 16
+#define LED_OUT_R 17
+
+void setup() {
+    pinMode(LED_OUT_B, OUTPUT);
+    pinMode(LED_OUT_R, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_OUT_B, HIGH);
+    delay(300); 
+    digitalWrite(LED_OUT_B, LOW);
+    
+    digitalWrite(LED_OUT_R, HIGH);
+    delay(300); 
+    digitalWrite(LED_OUT_R, LOW);
+}
